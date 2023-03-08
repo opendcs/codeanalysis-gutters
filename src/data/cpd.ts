4c5176ea3274ac987362a9ff516abff724a003e7
Mike Neilson <michael.a.neilson@usace.army.mil>
2023-03-08 21:42:53 +0000

    can read cpd.xml file.

---
+++ src/data/cpd.ts
@@ -0,0 +1,77 @@
+import * as xml from 'xml2js';
+import * as fs from 'fs';
+import * as vscode from 'vscode';
+
+/**
+ * Contains the duplication data and function to retrieve the ranges
+ */
+export class DuplicationData {
+    public readonly thisFile: string;
+    public readonly otherFiles: string[];
+    public readonly startLine: number;
+    public readonly endLine: number;
+    public readonly numTokens: number;
+
+    public constructor(thisFile:string,otherFiles: string[],startLine: number,
+                       endLine: number, numTokens: number) {
+        this.thisFile = thisFile;
+        this.otherFiles = otherFiles;
+        this.startLine = startLine;
+        this.endLine = endLine;
+        this.numTokens = numTokens;
+    }
+
+    /**
+     * @returns DecorationOptions containing the ranges and links to the other files
+     */
+    public getDecorationInformation() {
+        return {
+            hoverMessage: new vscode.MarkdownString("# This is duplicated with:\r\nOther files."),
+            range: new vscode.Range(this.startLine,0,this.endLine,0)
+        };
+    }
+}
+
+export class CPDCache {
+    private duplicateData: Array<DuplicationData>;
+
+    public constructor(cpdXmlFile: string) {
+        //this.fileInfoMap = new Map<String,DuplicationData[]>();
+        this.duplicateData = new Array<DuplicationData>();
+        var self = this;
+        var data = fs.readFileSync(cpdXmlFile, "utf-8");
+        xml.parseString(data, (err, cpdData) => {
+            if (err) {
+                throw err;
+            }
+            var duplicates = cpdData["pmd-cpd"]["duplication"];
+            Object.keys(duplicates).forEach( (value:string,idx: number) => {
+                var tokensDuplicate = Number.parseInt(duplicates[idx].$.tokens);
+                var xmlFiles = duplicates[idx]["file"];
+                var allFiles = new Array<string>();
+                Object.keys(xmlFiles).forEach( (value,idx) => {
+                    allFiles.push(xmlFiles[idx].$.path);
+                });
+
+                Object.keys(xmlFiles).forEach( (value:string, idx:number) => {
+                    var dupFile = xmlFiles[idx].$;
+                    var file = dupFile.path;
+                    var startLine = Number.parseInt(dupFile.line);
+                    var endLine = Number.parseInt(dupFile.endline);
+                    var otherFiles = new Array<string>();
+                    allFiles.forEach((path) => {
+                        if (path !== file) {
+                            otherFiles.push(path);
+                        }
+                    });
+                    var dupElement = new DuplicationData(file,otherFiles,startLine,endLine,tokensDuplicate);
+                    self.duplicateData.push(dupElement);
+                });
+            });
+        });
+    }
+
+    public getData() {
+        return this.duplicateData;
+    }
+}
--- src/gutter.ts
+++ src/gutter.ts
@@ -1,10 +1,16 @@
 import * as vscode from 'vscode';
+import { CPDCache } from './data/cpd';
+
 
 export function renderGutters(context: vscode.ExtensionContext) {
+
+    var data = new CPDCache("/home/mike/projects/pmd-cpd-gutters/pmd-gutters/examples/cpd/cpd.xml");    
+    console.log("Got the following information: ");
+    console.dir(data.getData());
     var decType = vscode.window.createTextEditorDecorationType({
         isWholeLine: true,
         rangeBehavior: vscode.DecorationRangeBehavior.ClosedClosed,
-        //backgroundColor: "red",
+        backgroundColor: "red",
         overviewRulerColor: "red",
         overviewRulerLane: vscode.OverviewRulerLane.Full
     });
