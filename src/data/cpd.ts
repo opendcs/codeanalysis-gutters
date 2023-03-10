be1e8574cc3fbf6c28dbe9acc3147f6b252b2c0c
Mike Neilson <michael.a.neilson@usace.army.mil>
2023-03-10 17:30:23 +0000

    links in hover text now work.

--- src/data/cpd.ts
+++ src/data/cpd.ts
@@ -2,17 +2,35 @@ import * as xml from 'xml2js';
 import * as fs from 'fs';
 import * as vscode from 'vscode';
 
+export class OtherFile {
+    public readonly file: vscode.Uri;
+    public readonly line: number;
+
+    public constructor(file: vscode.Uri, line: number) {
+        this.file = file;
+        this.line = line;
+    }
+
+    public linkText(): string {
+        return this.file.toString() + ":" + this.line.toFixed(0);
+    }
+
+    public link(): string {
+        return this.file.toString() + "#" + this.line.toFixed(0);
+    }
+}
+
 /**
  * Contains the duplication data and function to retrieve the ranges
  */
 export class DuplicationData {
     public readonly thisFile: string;
-    public readonly otherFiles: string[];
+    public readonly otherFiles: OtherFile[];
     public readonly startLine: number;
     public readonly endLine: number;
     public readonly numTokens: number;
 
-    public constructor(thisFile:string,otherFiles: string[],startLine: number,
+    public constructor(thisFile:string,otherFiles: OtherFile[],startLine: number,
                        endLine: number, numTokens: number) {
         this.thisFile = thisFile;
         this.otherFiles = otherFiles;
@@ -25,11 +43,11 @@ export class DuplicationData {
      * @returns DecorationOptions containing the ranges and links to the other files
      */
     public getDecorationInformation() {
-        var msg = new vscode.MarkdownString("# This is duplicated with: ");
-        this.otherFiles.forEach((fileName) => {
-            msg.appendText("- " + fileName);
+        var msg = new vscode.MarkdownString("# This is duplicated with:\r\n---\r\n\r\n");
+        this.otherFiles.forEach((file) => {
+            msg.appendMarkdown("- ["+ file.linkText() +"](" + file.link() + ")\r\n");
         });
-
+        //msg.isTrusted = true;
         return {
             hoverMessage: msg,
             range: new vscode.Range(this.startLine,0,this.endLine,0)
@@ -54,14 +72,31 @@ export class CPDFileHunter {
             files.forEach( (file) => {
                 vscode.workspace.fs.readFile(file)
                       .then( (data) => {
-                        if (data.toString().includes("<pmd-cpd>")) {
-                            console.log("Found pmd-cpd file: " + file.toString());
-                            callback(file);                            
-                        }
+                            if (data.toString().includes("<pmd-cpd>")) {
+                                console.log("Found pmd-cpd file: " + file.toString());
+                                callback(file);
+                            }
                       });
             });
         });
-    }    
+    }
+}
+
+/**
+ * Make sure we have a full path to the file
+ * @param file full or relative path to file
+ * @returns Uri to the file with workspace dir added if needed.
+ */
+function expandedUri(file: string): vscode.Uri {
+    var uri = vscode.Uri.file(file);
+    /** If the file is a relative assume it's from the workspace root
+         * and tweak as required
+         */
+    if (!file.startsWith("/") && vscode.workspace.workspaceFolders !== undefined) {
+        const workspaceDir = vscode.workspace.workspaceFolders[0].uri;
+        uri = vscode.Uri.file(workspaceDir.path + "/" + file);
+    }
+    return uri;
 }
 
 /**
@@ -73,8 +108,8 @@ export class CPDCache {
     private fileHunter: CPDFileHunter;
 
     public constructor() {
-        this.duplicateData = new Map<string,Array<DuplicationData>>();        
-        
+        this.duplicateData = new Map<string,Array<DuplicationData>>();
+
         var self = this;
         this.fileHunter = new CPDFileHunter((file) => {
             console.log("Registering file watchers");
@@ -99,7 +134,7 @@ export class CPDCache {
 
     private readData(file: vscode.Uri) {
         var self = this;
-        
+
         vscode.workspace.fs.readFile(file).then((data) => {
             xml.parseString(data, (err, cpdData) => {
                 if (err) {
@@ -109,9 +144,15 @@ export class CPDCache {
                 Object.keys(duplicates).forEach( (value:string,idx: number) => {
                     var tokensDuplicate = Number.parseInt(duplicates[idx].$.tokens);
                     var xmlFiles = duplicates[idx]["file"];
-                    var allFiles = new Array<string>();
+                    var allFiles = new Array<OtherFile>();
                     Object.keys(xmlFiles).forEach( (value,idx) => {
-                        allFiles.push(xmlFiles[idx].$.path);
+                        var xmlFile = xmlFiles[idx].$;
+                        allFiles.push(
+                            new OtherFile(
+                                expandedUri(xmlFile.path),
+                                Number.parseInt(xmlFile.line)
+                            )
+                        );
                     });
 
                     Object.keys(xmlFiles).forEach( (value:string, idx:number) => {
@@ -119,21 +160,14 @@ export class CPDCache {
                         var file = dupFile.path;
                         var startLine = Number.parseInt(dupFile.line);
                         var endLine = Number.parseInt(dupFile.endline);
-                        var otherFiles = new Array<string>();
+                        var otherFiles = new Array<OtherFile>();
                         allFiles.forEach((path) => {
-                            if (path !== file) {
+                            if (path.file.toString() !== expandedUri(file).toString()) {
                                 otherFiles.push(path);
                             }
                         });
-                        
-                        var uri = vscode.Uri.file(file);
-                        /** If the file is a relative assume it's from the workspace root
-                          * and tweak as required
-                          */
-                        if (!file.startsWith("/") && vscode.workspace.workspaceFolders !== undefined) {
-                            const workspaceDir = vscode.workspace.workspaceFolders[0].uri;
-                            uri = vscode.Uri.file(workspaceDir.path + "/" + file);
-                        }
+
+                        var uri = expandedUri(file);
                         var uriString = uri.toString();
                         var dupElement = new DuplicationData(file,otherFiles,startLine,endLine,tokensDuplicate);
                         if(!self.duplicateData.has(uriString)) {
@@ -164,7 +198,7 @@ export class CPDCache {
             /* try to match with shorter and shorter */
 
         }
-        
+
         return [];
     }
 }
